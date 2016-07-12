2ed633891c18e9872be47949d15d917728075f47
Paul Williamson <squarefrog@gmail.com>
2016-07-12 19:21:31 +0100

    Adds Last Updated menu item

--- ImageOfTheDay/Controllers/StatusMenuController.swift
+++ ImageOfTheDay/Controllers/StatusMenuController.swift
@@ -5,12 +5,14 @@ import Cocoa
 class StatusMenuController: NSObject {
     @IBOutlet weak var statusMenu: NSMenu!
     @IBOutlet weak var updateMenuItem: NSMenuItem!
+    @IBOutlet weak var lastUpdatedMenuItem: NSMenuItem!
 
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSSquareStatusItemLength)
 
     override func awakeFromNib() {
         statusItem.image = NSImage(named: "MenuIcon")
         statusItem.menu = statusMenu
+        lastUpdatedMenuItem.title = "Last updated: never"
     }
 
     @IBAction func updateClicked(sender: AnyObject) {
