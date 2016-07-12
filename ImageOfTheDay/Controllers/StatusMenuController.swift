eadd8fa6187e033d00c5cf81f975fe11724a93a4
Paul Williamson <squarefrog@gmail.com>
2016-07-12 19:21:06 +0100

    Adds Update menu item

--- ImageOfTheDay/Controllers/StatusMenuController.swift
+++ ImageOfTheDay/Controllers/StatusMenuController.swift
@@ -4,12 +4,16 @@ import Cocoa
 
 class StatusMenuController: NSObject {
     @IBOutlet weak var statusMenu: NSMenu!
+    @IBOutlet weak var updateMenuItem: NSMenuItem!
 
     let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSSquareStatusItemLength)
 
     override func awakeFromNib() {
         statusItem.image = NSImage(named: "MenuIcon")
         statusItem.menu = statusMenu
+    }
+
+    @IBAction func updateClicked(sender: AnyObject) {
     }
 
     @IBAction func quitClicked(sender: NSMenuItem) {
