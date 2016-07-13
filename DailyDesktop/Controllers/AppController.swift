cdee9275c14104df9ed7368f1fc6e6da79eaf0f7
Paul Williamson <squarefrog@gmail.com>
2016-07-13 18:39:17 +0100

    Creates App Controller class

---
+++ DailyDesktop/Controllers/AppController.swift
@@ -0,0 +1,11 @@
+//  Copyright © 2016 Paul Williamson. All rights reserved.
+
+import Foundation
+
+class AppController: NSObject {
+    @IBOutlet weak var menuController: StatusMenuController!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
